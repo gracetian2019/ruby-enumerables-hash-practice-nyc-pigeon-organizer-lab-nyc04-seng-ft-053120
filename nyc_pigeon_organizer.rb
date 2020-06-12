810a2a7a66bb1d26148cb0f724424a59a1c920b6
Grace Tian <gracetian4ever@gmail.com>
2020-06-12 02:32:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key,value),final_array|
+    value.each do |inner_key,names|
+      names.each do |name|
+        binding.pry
+    final_array
+  end
 end
+ nyc_pigeon_organizer(pigeon_data)
